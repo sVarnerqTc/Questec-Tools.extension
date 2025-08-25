ec0bb004699588b7449f99a9e218d20f9c91d575
sVarnerqTc <stephen.varner@questec.us>
2025-08-25 08:15:42 -0500

    Modified Test > Points From CSV

--- QuesTec_Tools_23.tab/Test.panel/Test.pulldown/Points From CSV.pushbutton/script.py
+++ QuesTec_Tools_23.tab/Test.panel/Test.pulldown/Points From CSV.pushbutton/script.py
@@ -1,6 +1,7 @@
 import csv
 import math
 from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB.Structure import StructuralType
 from Autodesk.Revit.UI import TaskDialog, TaskDialogCommonButtons
 from pyrevit import forms
 import os
@@ -24,45 +25,60 @@ def get_control_point_family():
 
 def transform_point(x, y, use_shared):
     if not use_shared:
+        # For project internal coordinates, use coordinates as-is
+        print("Using project internal coordinates - X: {}, Y: {}".format(x, y))
         return XYZ(x, y, 0)
     
-    # Get project location and position
-    project_location = doc.ActiveProjectLocation
-    project_position = project_location.GetProjectPosition(XYZ(0, 0, 0))
-    
-    # Get survey point and its position
-    survey_point = FilteredElementCollector(doc).OfCategory(
-        BuiltInCategory.OST_SharedBasePoint).FirstElement()
-    
-    # Get true north rotation
-    angle = project_position.Angle
-    
-    # Get survey point coordinates
-    east = project_position.EastWest
-    north = project_position.NorthSouth
-    
-    # Debug prints
-    print("Project Position Angle (radians): {}".format(angle))
-    print("Project Position East Offset (feet): {}".format(east))
-    print("Project Position North Offset (feet): {}".format(north))
-    print("Input coordinates - X: {}, Y: {}".format(x, y))
-
-    translated_x = x - east
-    translated_y = y - north
-    
-    # Apply rotation and translation
-    final_x = translated_x * math.cos(angle) - translated_y * math.sin(angle)
-    final_y = translated_x * math.sin(angle) + translated_y * math.cos(angle)
-    
-    # Apply survey point offset
-    #final_x = rotated_x - east
-    #final_y = rotated_y - north
-
-    print("Output coordinates - X: {}, Y: {}".format(final_x, final_y))
-    
-    return XYZ(final_x, final_y, 0)
-    
-    print("Output coordinates - X: {}, Y: {}".format(final_x, final_y))
+    try:
+        # For shared coordinates, we need to transform from survey coordinates to project coordinates
+        project_location = doc.ActiveProjectLocation
+        project_position = project_location.GetProjectPosition(XYZ(0, 0, 0))
+        
+        # Get survey point (shared base point)
+        survey_point = FilteredElementCollector(doc).OfCategory(
+            BuiltInCategory.OST_SharedBasePoint).FirstElement()
+        
+        if not survey_point:
+            print("Warning: Survey point not found, using coordinates as project internal")
+            return XYZ(x, y, 0)
+        
+        # Get the transformation parameters
+        angle = project_position.Angle  # True north rotation angle
+        east_offset = project_position.EastWest  # Project base point offset from survey point (East)
+        north_offset = project_position.NorthSouth  # Project base point offset from survey point (North)
+        
+        # Get survey point position in project coordinates
+        survey_point_pos = survey_point.get_Parameter(BuiltInParameter.BASEPOINT_EASTWEST_PARAM).AsDouble()
+        survey_point_north = survey_point.get_Parameter(BuiltInParameter.BASEPOINT_NORTHSOUTH_PARAM).AsDouble()
+        
+        # Debug prints
+        print("Project Position Angle (radians): {}".format(angle))
+        print("Project Position East Offset (feet): {}".format(east_offset))
+        print("Project Position North Offset (feet): {}".format(north_offset))
+        print("Survey Point East (feet): {}".format(survey_point_pos))
+        print("Survey Point North (feet): {}".format(survey_point_north))
+        print("Input shared coordinates - X: {}, Y: {}".format(x, y))
+        
+        # Transform from shared coordinates to project coordinates
+        # Step 1: Apply rotation (counter-clockwise rotation by negative angle)
+        cos_angle = math.cos(-angle)
+        sin_angle = math.sin(-angle)
+        rotated_x = x * cos_angle - y * sin_angle
+        rotated_y = x * sin_angle + y * cos_angle
+        
+        # Step 2: Apply translation (subtract the offsets to convert to project coordinates)
+        final_x = rotated_x - east_offset
+        final_y = rotated_y - north_offset
+        
+        print("After rotation - X: {}, Y: {}".format(rotated_x, rotated_y))
+        print("Final project coordinates - X: {}, Y: {}".format(final_x, final_y))
+        
+        return XYZ(final_x, final_y, 0)
+        
+    except Exception as e:
+        print("Error in coordinate transformation: {}".format(str(e)))
+        print("Falling back to project internal coordinates")
+        return XYZ(x, y, 0)
 
 def verify_csv_headers(headers):
     required = ['pointNumber', 'X', 'Y', 'Z', 'Description', 'Layer']
@@ -128,14 +144,14 @@ def main():
                 point = XYZ(point.X, point.Y, level.Elevation)
                 
                 instance = doc.Create.NewFamilyInstance(
-                    point, family_symbol, level, Structure.StructuralType.NonStructural)
+                    point, family_symbol, level, StructuralType.NonStructural)
                 
                  # Set Mark parameter
                 mark_param = instance.LookupParameter('Mark')
                 if mark_param:
-                    mark_param.Set(str(row[0]))
+                    mark_param.Set(str(row[0]))  # Point number from 1st column
                 else:
-                  print("Could not set Mark parameter for point {}".format(row['pointNumber']))
+                    print("Could not set Mark parameter for point {}".format(row[0]))
 
                 if override_text:
                     instance.get_Parameter(
